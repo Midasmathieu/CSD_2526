0ec4bbf48c5031e0b9165ad56f89dc2582f7a009
Midas <midasm@icloud.com>
2025-09-29 16:45:50 +0200

    added degrees scaling to pendulum and fixed bug in pseudocode

--- BLOK_1/rythm_generator/pygame_visual/pendulum visual/pendulum.py
+++ BLOK_1/rythm_generator/pygame_visual/pendulum visual/pendulum.py
@@ -4,13 +4,21 @@ pygame.init()
 windowWidth = 800
 windowHeight = 800
 xZero = windowWidth / 2
-yZero = windowHeight / 2
-r1 = 100
-r2 = 100
-m1 = 10
-m2 = 10
-a1 = 1
-a2 = 0
+yZero = windowHeight / 2 - 20
+
+#credits for all the math implementation of the pendulum to the coding train (https://thecodingtrain.com/challenges/93-double-pendulum)
+degrees1 = float(input("where do you want pendulum1 to start in degrees?: "))
+a1 = degrees1 * math.pi / 180
+degrees2 = float(input("where do you want pendulum2 to start in degrees?: "))
+a2 = degrees1 * math.pi / 180
+#TODO: take modulo of degrees so if degrees > 360 the pendulum will still trigger the if else statements
+time_seconds = float(input("how long do you want the double pendulum to run in seconds"))
+time = time_seconds * 60
+
+r1 = 180
+r2 = 180
+m1 = 1
+m2 = 1
 a1_v = 0
 a2_v = 0
 a1_a = 0
@@ -19,8 +27,11 @@ g = 1
 
 running = True
 clock = pygame.time.Clock()
-delta_time = 0.1
+timer = 0
 screen = pygame.display.set_mode((windowWidth, windowHeight))
+preva1 = a1
+x1 = 0
+prevx1 = 0
 
 # alles binnen while running wordt op loop uitgevoerdt.
 while running:
@@ -31,12 +42,14 @@ while running:
     y2 = y1 + r2 * math.cos(a2)
     line1 = pygame.draw.line(screen, (150, 150, 150), (xZero, yZero), (x1, y1))
     line2 = pygame.draw.line(screen, (150, 150, 150), (x1, y1), (x2, y2))
-    ball1 = pygame.draw.circle(screen, (100, 100, 100), (x1, y1), m1)
-    ball2 = pygame.draw.circle(screen, (100, 0, 100), (x2, y2), m2)
+    # ball1 = pygame.draw.circle(screen, (100, 100, 100), (x1, y1), m1)
+    # ball2 = pygame.draw.circle(screen, (100, 0, 100), (x2, y2), m2)
     a1 = a1 + a1_v
-    a1_v = a1_v + a1_a * delta_time
+    a1_v = a1_v + a1_a
     a2 = a2 + a2_v
-    a2_v = a2_v + a2_a * delta_time
+    a2_v = a2_v + a2_a
+    a1_v = a1_v * 0.995
+    a2_v = a2_v * 0.995
 
     num1 = -g * (2 * m1 + m2) * math.sin(a1)
     num2 = -m2 * g * math.sin((a1 - 2 * a2))
@@ -51,23 +64,9 @@ while running:
     num4 = a2_v * a2_v * r2 * m2 * math.cos(a1 - a2)
     dev = r2 * (2 * m1 + m2 - m2 * math.cos((2 * a1 - 2 * a2)))
     a2_a = (num1 * (num2 + num3 + num4)) / den
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if x1 > 0 and prevx1 < 0:
+        # kick.append(millisec)
+        print(check)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -75,7 +74,18 @@ while running:
 
     pygame.display.flip()
 
-    delta_time = clock.tick(60)
-    delta_time = max(0.001, min(0.1, delta_time))
+    clock.tick(60)
+    timer = timer + 1
+    # print(a1 % math.pi)
+    if (a1 % math.pi) > 0 and (preva1 % math.pi) < 0:
+        print("aaaaaaaaaaaaä")
+
+    if timer > time:
+        running = False
+
+    preva1 = a1
+    prevx1 = x1
 
 pygame.quit()
+
+next_step = input("DOYAWANNATEEKDANEXTSTEP? ")
