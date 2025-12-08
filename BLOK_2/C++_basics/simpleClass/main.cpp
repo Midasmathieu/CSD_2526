8ce45103709a8407b287cad329bb7a08a517ae43
Midas <midasm@icloud.com>
2025-12-08 16:33:03 +0100

    little class for exercise

---
+++ BLOK_2/C++_basics/simpleClass/main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+
+class Synth{
+private:
+  int freq = 0;
+
+public:
+  void setFreq(float freq){
+    if(freq < 12){
+      this->freq = 12;
+    }
+    else if(freq > 24){
+      this->freq = 24;
+    }
+    else{
+      this->freq = freq;
+    }
+  }
+
+  float getFreq(){
+    return freq;
+  }
+
+  void play(){
+    std::cout << "playing right now\n";
+  }
+  void stop(std::string note){
+    std::cout << "stopping right now " << note;
+
+  }
+};
+
+class AdditiveSynth : public Synth{
+private:
+  int voices;
+  // int calculateOvertone(int freq, int voices){
+  //   freq
+  // }
+
+public:
+  int setVoices(int voices){
+    this->voices = voices;
+    return 0;
+  }
+  int getVoices(){
+    return voices;
+  }
+};
+
+int main(){
+  AdditiveSynth Synth1;
+  Synth1.setFreq(14.0);
+
+  Synth1.play();
+
+  std::cout << Synth1.getFreq()
+               << "\n";
+  Synth1.stop("high note\n");
+
+  return 0;
+}
