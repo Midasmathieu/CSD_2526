611eb431835300cd0159181f2a58e2a1a968a352
Midas <midasm@icloud.com>
2026-03-10 16:35:53 +0100

    finished bare bone biquads (prikle and cookbook)

--- BLOK_3/codeClinics/cc42/main.cpp
+++ BLOK_3/codeClinics/cc42/main.cpp
@@ -21,11 +21,10 @@ struct CustomCallback : AudioCallback {
 
     void prepare(int sampleRate, int blockSize) override {
         saw.prepare(sampleRate);
-        std::cout << sampleRate << std::endl;
         saw.setFrequency(110.0f);
         lfo.prepare(sampleRate);
         lfo.setFrequency(0.3f);
-
+        filter.calculateCoefficients(1000.0f, 10.0f);
 
 
     }
@@ -37,24 +36,6 @@ struct CustomCallback : AudioCallback {
         for (auto sample = 0u; sample < numFrames; ++sample) {
             saw.tick();
             lfo.tick();
-            float w0 = (2.0f*M_PI*3000.0f+lfo.getSample()*1000)/static_cast<float>(48000);
-            float cosW0 = cos(w0);
-            float sinW0 = sin(w0);
-            // std::cout << w0 << ", " << sinW0 << std::endl;
-            float alfa = sinW0/(2.0f*1.0f);
-            float b1 = 1.0f-cosW0;
-            float b0 = b1/2.0f;
-            float b2 = b0;
-            float a0 = 1.0f+alfa;
-            float a1 = -2.0f*cosW0;
-            float a2 = 1.0f-alfa;
-            // std::cout << "check deze: " << a1 << ", " << a2 << ", " << b0 << ", " << b1 << ", " << b2 << std::endl;
-            filter.setA1Coefficient(a0);
-            filter.setA1Coefficient(a1);
-            filter.setA2Coefficient(a2);
-            filter.setB0Coefficient(b0);
-            filter.setB1Coefficient(b1);
-            filter.setB2Coefficient(b2);
             const float oscSample = saw.getSample() * 0.2f;
             float filteredOutput = filter.process(oscSample);
             if(filteredOutput >= 1.0) { filteredOutput = 1.0; std::cout << "clipping \n";
@@ -68,7 +49,7 @@ struct CustomCallback : AudioCallback {
 
     Saw saw {200};
     Saw lfo {0.5};
-    Biquad filter;
+    PirkleBiquad filter;
 };
 
 #define Delta_Sequence 0
