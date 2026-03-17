f36c96eb3e50d6ed1fce6a0eaa83df62ebfbaab7
Midas <midasm@icloud.com>
2026-03-17 14:14:03 +0100

    calculate readH

--- BLOK_3/TEST/Plugin_Focus/EffectsChain.h
+++ BLOK_3/TEST/Plugin_Focus/EffectsChain.h
@@ -10,8 +10,9 @@ public:
     EffectsChain() {}
     void prepareToPlay(float sampleRate, int numSamplesPerBlock){
         std::cout << sampleRate << std::endl;
-        circularBuffer.resetSize(96000);
-        circularBuffer.setDistanceRW(24003);
+        circularBuffer.resetSize(192000);
+        circularBuffer.setDistanceRW(200);
+        circularBuffer.m_calculatePhaseStep();
     }
 
 
@@ -23,10 +24,11 @@ public:
             for (int sample = 0; sample < buffer.getNumSamples(); ++sample){
                 circularBuffer.tick();
                 float tempInput = inputChannel[sample];
+
                 circularBuffer.write(tempInput);
                 // std::cout << tempInput << std::endl;
-                float sampletje = circularBuffer.read();
-                outputChannel[sample] = sampletje + tempInput;
+                sampletje = circularBuffer.read();
+                outputChannel[sample] = sampletje;
                 // std::cout << sampletje << std::endl;
             }
         }
@@ -34,12 +36,24 @@ public:
 
     void setParameter(float parameter){
 
-        delay.setFeedback(parameter*0.9);
+        // delay.setFeedback(parameter*0.9);
+
+
+        if(prevParameter != parameter){
+          //static_cast fuckt dingen op in classes gebruik iets anders
+          std::cout << parameter << std::endl;
+          unsigned int delayTime = parameter * 95000.0 + 1000.0;
+        }
+
+        prevParameter = parameter;
       }
 
 private:
+  float  prevParameter = -1;
+  float sampletje;
   WaveShaper waveShaper;
   Delay delay;
   float tempSample;
   CircularBuffer circularBuffer;
+  float feedback;
 };
--- BLOK_3/TEST/Plugin_Focus/csd_lib/effects/CircularBuffer.h
+++ BLOK_3/TEST/Plugin_Focus/csd_lib/effects/CircularBuffer.h
@@ -16,6 +16,7 @@ public:
     releaseBuffer();
   }
 
+
   void allocateBuffer() {
     m_buffer = (float*)malloc(m_size * sizeof(float));
     memset(m_buffer, 0, m_size * sizeof(float));
@@ -26,10 +27,12 @@ public:
   }
 
   inline void tick() {
-    incrReadH();
+    calculateReadH();
     incrWriteH();
     // std::cout << "test" << std::endl;
-    incrTimer();
+    incrPhase();
+    // std::cout << m_headPhase << std::endl;
+    // readH0.incrH();
   }
 
   inline float read() {
@@ -45,6 +48,7 @@ public:
 
   void resetSize(unsigned int size) {
     m_size = size - sizeof(float);
+    // readH0.setSize(m_size);
     releaseBuffer();
     allocateBuffer();
     std::cout << m_size << std::endl;
@@ -52,8 +56,8 @@ public:
 
   void setDistanceRW(unsigned int distanceRW) {
     m_distanceRW = distanceRW;
-    m_readH = m_writeH - m_distanceRW + m_size;
-    wrapH(m_readH);
+    // m_readH = m_writeH - m_distanceRW + m_size;
+    // wrapH(m_readH);
     std::cout << "setDistanceRW\n";
   }
 
@@ -61,47 +65,65 @@ public:
     return m_distanceRW;
   }
 
+  void m_calculatePhaseStep() {
+    // m_phaseStep = 1.0/(static_cast<float>(m_sampleRate) * static_cast<float>(m_phaseDurMillis));
+    m_phaseStep = 1.0/static_cast<float>(m_phaseDur);
+
+    std::cout << "goyly shit we got phaseStep:  " << m_phaseStep << std::endl;
+  }
+
+  void setGrainSize(int grainsize){
+    m_grainSize = grainsize;
+  }
 
 private:
-  inline void incrReadH() {
-    m_readH--;
+
+  inline void calculateReadH() {
+    float backward = m_headPhase * static_cast<float>(m_grainSize)*2;
+    // m_readH = m_writeH - static_cast<int>(backward);
+    int m_intReadH = static_cast<int>(m_writeH) - static_cast<int>(m_distanceRW) - static_cast<int>(backward);
+    // std::cout << "reqad head prev wrap: " << m_readH << "headPhase : " << m_headPhase << std::endl;
+    revWrapH(m_intReadH);
+    m_readH = static_cast<unsigned int>(m_intReadH);
     wrapH(m_readH);
-    revWrapH(m_readH);
-    // std::cout << "readH: " << m_readH << std::endl;
+    // std::cout << "backward: " << backward << "    head: " << m_readH << std::endl;
+    std::cout << "reqad head: " <<  m_readH << std::endl; //" m_distanceRW:  " << m_distanceRW << "backward:    "  << backward << std::endl;
   }
 
   inline void incrWriteH() {
     m_writeH++;
     wrapH(m_writeH);
+    std::cout << "writhead :  " << m_writeH << std:: endl;
   }
 
-  inline void incrTimer() {
-    m_timer++;
-    if(m_timer >= m_distanceRW){
-      m_timer = m_timer - m_distanceRW;
-      m_readH = m_readH + m_distanceRW * 2;
-      wrapH(m_readH);
-      revWrapH(m_readH);
-    }
+  inline void incrPhase() {
+    m_headPhase += m_phaseStep;
+    if(m_headPhase > 1.0) { m_headPhase -= 1.0; std::cout << "phasereset" << std::endl;}
+    // std::cout << m_headPhase << std::endl;
   }
-
 
   inline void wrapH(unsigned int& head) {
-    if (head >= m_size) { head = head - m_size;
-    std::cout << " here we go: wrap\n"; }
+    if (head > m_size) { head -= m_size;
+    std::cout << " here we go: wrap: " << head << std::endl; }
   }
 
-  inline void revWrapH(unsigned int& head) {
-    if (head <= 0) { head = head + m_size; std::cout << "wrapreverse\n";}
+  inline void revWrapH(int& head) {
+    if (head < 0) { head = head + m_size; std::cout << "wrapreverse\n";}
   }
 
+
+  // ReadH readH0;
   unsigned int m_size = 48000;
   unsigned int m_readH;
-  unsigned int m_writeH;
+  unsigned int m_writeH = 0;
   unsigned int m_distanceRW;
   unsigned int m_timer = { 0 };
+  int m_grainSize =  48000;
+  int m_phaseDur = 48000;
+  float m_headPhase = 0.0;
+  int m_sampleRate = 48000;
+  float m_phaseStep; //= 1/(m_sampleRate * m_grainSize);
   float* m_buffer;
-
 
 };
 
