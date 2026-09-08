77e95c0b948f8dbfe4558e9aa013ed20ff5df362
Midas <midasm@icloud.com>
2026-09-08 21:58:21 +0200

    finished allpass filter

--- PROJ_3/allPassFilter/main.cpp
+++ PROJ_3/allPassFilter/main.cpp
@@ -9,12 +9,29 @@ class Allpass
 
   void applyEffect(const float& input, float& output)
   {
-    output = input * 0.167772 + sampleMinusOne;
-    sampleMinusOne = input;
+    output = flexInput * 0.167772 - sampleMinusEight;
+    sampleMinusEight = sampleMinusSeven;
+    sampleMinusSeven = sampleMinusSix;
+    sampleMinusSix = sampleMinusFive;
+    sampleMinusFive = sampleMinusFour;
+    sampleMinusFour = sampleMinusThree;
+    sampleMinusThree = sampleMinusTwo;
+    sampleMinusTwo = sampleMinusOne;
+    sampleMinusOne = flexInput;
+    flexInput = input + 0.167772 * sampleMinusEight;
     std::cout << output << std::endl;
   }
+  float flexInput =        { 0.0f };
+  float summedInput =      { 0.0f };
+  float sampleMinusOne =   { 0.0f };
+  float sampleMinusTwo =   { 0.0f };
+  float sampleMinusThree = { 0.0f };
+  float sampleMinusFour =  { 0.0f };
+  float sampleMinusFive =  { 0.0f };
+  float sampleMinusSix =   { 0.0f };
+  float sampleMinusSeven = { 0.0f };
+  float sampleMinusEight = { 0.0f };
 
-  float sampleMinusOne = { 0.0f };
 };
 
 
@@ -25,7 +42,7 @@ int main()
   float inputSignal = 1.0f;
   filter.applyEffect(inputSignal, filteredSignal);
   inputSignal = 0.0f;
-  for(int i = 0; i <= 10; i++)
+  for(int i = 0; i <= 20; i++)
   {
     filter.applyEffect(inputSignal, filteredSignal);
   }
