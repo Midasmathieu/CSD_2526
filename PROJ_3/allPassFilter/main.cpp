0ffddd46e3c851d03ef475d077b2a71cc9730566
Midas <midasm@icloud.com>
2026-09-09 10:41:56 +0200

    added commentsi, samplecounter and CMakeLists.txt

--- PROJ_3/allPassFilter/main.cpp
+++ PROJ_3/allPassFilter/main.cpp
@@ -6,6 +6,7 @@ class Allpass {
   Allpass() { std::cout << "Allpass constructor" << std::endl; }
   ~Allpass() { std::cout << "Allpass destructor" << std::endl; }
 
+  //filter dsp calculations
   void applyEffect(const float& input, float& output) {
     feedbackInput = input + 0.167772f * sampleMinusEight;
     output = feedbackInput * 0.167772f - sampleMinusEight;
@@ -17,29 +18,33 @@ class Allpass {
     sampleMinusThree = sampleMinusTwo;
     sampleMinusTwo = sampleMinusOne;
     sampleMinusOne = feedbackInput;
-    std::cout << output << std::endl;
+    // added cout for impulse response
+    std::cout << sampleNumber << ": " << output << std::endl;
+    sampleNumber++;
   }
-
-  float feedbackInput =        { 0.0f };
-  float summedInput =      { 0.0f };
-  float sampleMinusOne =   { 0.0f };
-  float sampleMinusTwo =   { 0.0f };
-  float sampleMinusThree = { 0.0f };
-  float sampleMinusFour =  { 0.0f };
-  float sampleMinusFive =  { 0.0f };
-  float sampleMinusSix =   { 0.0f };
-  float sampleMinusSeven = { 0.0f };
-  float sampleMinusEight = { 0.0f };
+  //buffer floats (could also have been circulair buffer but this is easier
+  float feedbackInput     = { 0.0f };
+  float summedInput       = { 0.0f };
+  float sampleMinusOne    = { 0.0f };
+  float sampleMinusTwo    = { 0.0f };
+  float sampleMinusThree  = { 0.0f };
+  float sampleMinusFour   = { 0.0f };
+  float sampleMinusFive   = { 0.0f };
+  float sampleMinusSix    = { 0.0f };
+  float sampleMinusSeven  = { 0.0f };
+  float sampleMinusEight  = { 0.0f };
+  int sampleNumber        = { 0 };
 };
 
 
 int main() {
+  // make filter and run impulse response signal through
   Allpass filter;
   float filteredSignal = 0.0f;
   float inputSignal = 1.0f;
   filter.applyEffect(inputSignal, filteredSignal);
   inputSignal = 0.0f;
-  for (int i = 0; i <= 20; i++) {
+  for (int i = 0; i <= 21; i++) {
     filter.applyEffect(inputSignal, filteredSignal);
   }
 }
